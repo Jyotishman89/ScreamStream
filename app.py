36dc524a89f521b804b2746f8e1de9e2a19ff40c
Jyotishman89 <jyotishman45@gmail.com>
2026-06-23 12:49:31 +0530

    Header backdrop: full poster marquee with float instead of vertical strips

--- app.py
+++ app.py
@@ -1337,7 +1337,7 @@ def admin_required(view):
         return view(*args, **kwargs)
     return wrapped
 
-ASSET_VERSION = "6"
+ASSET_VERSION = "7"
 BACKDROP_COLUMNS = 8
 BACKDROP_TILES_PER_COL = 5
 _backdrop_cache = {"at": 0.0, "cols": None}
